7c6a6bc0158270c007fc8fd5a6ecd3481da8f552
Omar Ahmed <opop2omar@gmail.com>
2026-04-09 20:46:52 +0200

    Update google-success.js

--- Web/frontend/src/components/google-success.js
+++ Web/frontend/src/components/google-success.js
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { useUser } from "./UserContext";
 
 const GoogleSuccess = () => {
   const navigate = useNavigate();
+  const { setUser } = useUser();
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -11,6 +13,8 @@ const GoogleSuccess = () => {
         const res = await axios.get("https://sitx-backend-new.onrender.com/profile", {
           withCredentials: true,
         });
+
+        setUser(res.data);
 
         if (res.data?.role === "admin") navigate("/admin");
         else navigate("/");
@@ -20,7 +24,7 @@ const GoogleSuccess = () => {
     };
 
     fetchUser();
-  }, [navigate]);
+  }, [navigate, setUser]);
 
   return <p>Logging you in via Google...</p>;
 };
